8f3a4efa1fd6bedc87bee8bcd2021a6c8520541c
Кирилл Донцов <kirill.dontsov@kmslh.com>
2025-10-27 17:46:33 +0300

    fix: avito api
    AC-001

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,4 +1,4 @@
-export const DOMAIN = '127.0.0.1:8080';
+export const DOMAIN = 'localhost:8080';
 // export const DOMAIN = 'xn--90ab9accji9e.xn--p1ai';
 export const BACKEND_PORT = `https://${DOMAIN}`;
 
