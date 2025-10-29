1e867b83a6f6c688088b0d42d2ccd7950618473a
Кирилл Донцов <kirill.dontsov@kmslh.com>
2025-10-29 13:52:18 +0300

    feat: avito api and pagination
    AC-001

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,5 +1,5 @@
 // export const DOMAIN = 'localhost:8080';
-export const DOMAIN = 'xn--90ab9accji9e.xn--p1ai';
+export const DOMAIN = 'sunseven.ru';
 export const BACKEND_PORT = `https://${DOMAIN}`;
 
 export const AVITO_ITEMS_PAGINATION_LIMIT = 100;
