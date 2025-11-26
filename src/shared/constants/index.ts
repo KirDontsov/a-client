550fdcb43debcdc61d8cc3ccce7a98addf098e58
Кирилл Донцов <kirill.dontsov@kmslh.com>
2025-11-26 14:24:08 +0300

    feat: auth flow change to headers
    AC-001

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,8 +1,8 @@
 // Use environment-specific domain
 export const DOMAIN =
   process.env.NODE_ENV === 'production'
-    ? 'xn--90ab9accji9e.xn--p1ai' // Production domain
-    : 'localhost:8080';
+    ? 'sunseven.ru' // Production domain
+    : 'localhost:8081';
 export const BACKEND_PORT = process.env.NODE_ENV === 'production' ? `https://${DOMAIN}` : `http://${DOMAIN}`;
 
 export const AVITO_ITEMS_PAGINATION_LIMIT = 100;
